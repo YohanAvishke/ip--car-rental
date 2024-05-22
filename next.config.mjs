0e20d130829019a6b769c12c5add894257043a13
YohanAvishke <yaediriweera@gmail.com>
2024-05-22 13:43:48 +1000

    Initialise next js project with shadcn

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,4 @@
-/** @type {import("next").NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com"
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com"
-      }
-    ]
-  }
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-export default nextConfig
+export default nextConfig;
