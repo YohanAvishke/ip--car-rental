afd474dd351234cc02820db675446c31ea57b749
YohanAvishke <yaediriweera@gmail.com>
2024-05-30 12:19:42 +1000

    Fix layout and finalise structure

--- app/ui/home/side-nav.tsx
+++ app/ui/home/side-nav.tsx
@@ -16,49 +16,45 @@ import {
 } from "@/app/ui/shadcn/sheet"
 
 const Sidebar = () => (
-  <div className="hidden md:flex flex-col gap-2 h-full max-h-screen">
-    <div className="flex h-14 lg:h-16 items-center justify-end bg-muted/40 border-b px-4 lg:px-6">
-      <Sheet>
-        <SheetTrigger asChild>
-          {/*Open button*/}
-          <Button variant="outline" size="icon">
-            <Menu className="h-5 w-5" />
-          </Button>
-        </SheetTrigger>
-        <SheetContent side="left">
-          <SheetHeader>
-            <SheetTitle>Drive Easy</SheetTitle>
-            <SheetDescription>
-              Filter by car models
-            </SheetDescription>
-          </SheetHeader>
-          <div className="flex flex-col pt-10">
-            <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
-              <HomeIcon className="h-5 w-5" />
-              Dashboard
-            </Link>
-            <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
-              <ShoppingCart className="h-5 w-5" />
-              Orders
-              <Badge className="ml-auto flex h-6 w-6 items-center justify-center rounded-full">6</Badge>
-            </Link>
-            <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
-              <Package className="h-5 w-5" />
-              Products
-            </Link>
-            <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
-              <Users className="h-5 w-5" />
-              Customers
-            </Link>
-            <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
-              <LineChart className="h-5 w-5" />
-              Analytics
-            </Link>
-          </div>
-        </SheetContent>
-      </Sheet>
-    </div>
-  </div>
+  <Sheet>
+    <SheetTrigger asChild>
+      {/*Open button*/}
+      <Button variant="outline" size="icon">
+        <Menu className="h-5 w-5" />
+      </Button>
+    </SheetTrigger>
+    <SheetContent side="left">
+      <SheetHeader>
+        <SheetTitle>Drive Easy</SheetTitle>
+        <SheetDescription>
+          Filter by car models
+        </SheetDescription>
+      </SheetHeader>
+      <div className="flex flex-col pt-10">
+        <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
+          <HomeIcon className="h-5 w-5" />
+          Dashboard
+        </Link>
+        <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
+          <ShoppingCart className="h-5 w-5" />
+          Orders
+          <Badge className="ml-auto flex h-6 w-6 items-center justify-center rounded-full">6</Badge>
+        </Link>
+        <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
+          <Package className="h-5 w-5" />
+          Products
+        </Link>
+        <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
+          <Users className="h-5 w-5" />
+          Customers
+        </Link>
+        <Link href="#" className="flex items-center gap-4 py-2.5 hover:bg-muted">
+          <LineChart className="h-5 w-5" />
+          Analytics
+        </Link>
+      </div>
+    </SheetContent>
+  </Sheet>
 )
 
 export default Sidebar
--- app/ui/home/top-nav.tsx
+++ app/ui/home/top-nav.tsx
@@ -1,34 +1,29 @@
 import Link from "next/link"
-import {
-  Package2,
-  Search
-} from "lucide-react"
+import { Search } from "lucide-react"
 import { Input } from "@/app/ui/shadcn/input"
 import Image from "next/image"
 import logo from "@/public/logo.png"
 import { Button } from "@/app/ui/shadcn/button"
 
 const TopNav = () => (
-  <header className="flex h-16 lg:h-18 items-center border-b bg-muted/40 px-4 lg:px-6">
-    <div className="flex items-center w-full">
-      <Link href="/" className="flex items-center gap-2 font-semibold">
-        <Image src={logo} width={50} height={50} alt="Logo" />
-        <span>Drive Easy</span>
-      </Link>
-      <div className="flex-1 mx-auto max-w-md">
-        <form>
-          <div className="relative">
-            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-            <Input
-              type="search"
-              placeholder="Search Cars..."
-              className="w-full bg-background pl-8 shadow-none"
-            />
-          </div>
-        </form>
-      </div>
-      <Button className="ml-auto">Reserve</Button>
+  <header className="flex items-center h-16">
+    <Link href="/" className="flex items-center gap-2 font-semibold">
+      <Image src={logo} width={60} height={60} alt="Logo" />
+      <span>Drive Easy</span>
+    </Link>
+    <div className="flex-1 mx-auto max-w-md">
+      <form>
+        <div className="relative">
+          <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+          <Input
+            type="search"
+            placeholder="Search Cars..."
+            className="w-full bg-background pl-8 shadow-none"
+          />
+        </div>
+      </form>
     </div>
+    <Button className="ml-auto">Reserve</Button>
   </header>
 )
 
